7ce166e6682e18c3c3e839dffbeaab3a1b4520b8
stevenasindarto <50313059+stevenasindarto@users.noreply.github.com>
2019-06-14 11:56:15 +0700

    Index
    Added a folder for assets and javascript, which is for the pictures and how to automatically transition them in the index screen.

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+var index = 0;
+slideshow();
+
+function slideshow() {
+  var i;
+  var image = document.getElementsByClassName("images");
+  var length = image.length;
+  for (i = 0; i < length; i++) image[i].style.display = "none"; 
+  index++;
+  if (index > length) {index = 1} 
+  image[index-1].style.display = "block"; 
+  setTimeout(slideshow, 2000);
+}
